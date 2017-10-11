97cc5005ddffdabd6a8508a1f67a2ea08bc8e2af
Henry Wainaina <wainainah@gmail.com>
2017-10-11 22:54:06 +0100

    'Canvas'

--- app/components/portal-header.js
+++ app/components/portal-header.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames:['top'],
   // tagName:'heander',
-  didInsertElement() {
-    this._super(...arguments);
-    var height= screen.height - 100;
-    Ember.$(".top").height(height);
-    Ember.$('[data-toggle="offcanvas"]').click(function(){
-       Ember.$("#navigation").toggleClass("hidden-xs");
-   });
-  }
-  
+  // didInsertElement() {
+  //   this._super(...arguments);
+  //   var height= screen.height - 100;
+  //   Ember.$(".top").height(height);
+  //   Ember.$('[data-toggle="offcanvas"]').click(function(){
+  //      Ember.$("#navigation").toggleClass("hidden-xs");
+  //  });
+  // }
+
 
 });
