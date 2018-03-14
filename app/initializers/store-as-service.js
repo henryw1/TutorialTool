b3daabbb7e5851e0bc0314c137d9aeb87d515598
Henry Wainaina <wainainah@gmail.com>
2018-03-14 12:28:48 +0000

    back

--- app/initializers/store-as-service.js
+++ app/initializers/store-as-service.js
@@ -1,5 +1,4 @@
 export function initialize(application) {
-  // application.inject('route', 'foo', 'service:foo');
   application.inject('component', 'store', 'service:store');
 }
 
@@ -9,9 +8,4 @@ export default {
    initialize: function(container, application) {
     application.inject('service:mtg-level-service', 'store', 'store:main');
   }
- //  initialize: function(container, application) {
- //   var store = container.lookup('store:main')
- //   container.register('service:store', store, { instantiate: false });
- // }
-
 };
